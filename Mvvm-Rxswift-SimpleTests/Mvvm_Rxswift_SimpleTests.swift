c2da6abbf6f026d55fcdfbf9f7535cffe4f98604
LeeGiTaek <coolfamily77@gmail.com>
2021-02-02 10:26:58 +0900

    Initial Commit

---
+++ Mvvm-Rxswift-SimpleTests/Mvvm_Rxswift_SimpleTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mvvm_Rxswift_SimpleTests.swift
+//  Mvvm-Rxswift-SimpleTests
+//
+//  Created by gitaeklee on 2021/02/02.
+//
+
+import XCTest
+@testable import Mvvm_Rxswift_Simple
+
+class Mvvm_Rxswift_SimpleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
